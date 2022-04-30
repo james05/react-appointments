3b31bacc2089521cdfad48183f805e6f369a389e
James Brocklebank <jgbrockl@uwaterloo.ca>
2022-04-30 15:01:30 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { BiCalendar } from "react-icons/bi";
+import Search from "./components/Search";
+import AddAppointment from "./components/AddAppointment";
+
+import appointmentList from "./data.json";
+import AppointmentDetails from "./components/AppointmentDetails";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App container mx-auto mt-3 font-thin">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="text-5xl mb-3">
+          <BiCalendar className="inline-block text-red-400 align-top" />Your Appointments
+        </h1>
+
+        <AddAppointment />
+
+        <Search />
       </header>
+
+
+      <ul className="divide-y divide-gray-200">
+        {appointmentList.map(appointment => (
+          <AppointmentDetails key={appointment.id} appointment={appointment} />
+        ))}
+      </ul>
     </div>
   );
 }
